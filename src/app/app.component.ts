1f40ebcaca377f6d2daa81fde53e340440bcd491
DAXILSAXENA <daxildaxil@gmail.com>
2021-08-23 04:46:06 +0530

    [Daxil] Added:Display message using One Way Data Binding.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,15 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hello-world';
-  imgUrl="../assets/BL_logo_square_jpg.jpg";
-  url="https://www.bridgelabz.com"
 
   ngOnInit(): void {
-    this.title = "Hello from Bridgelabz";
-  }
-
-  onClick($event: any){
-    console.log("Save button is clicked!",$event);
-    window.open(this.url,"_blank");
-  }
+    this.title = "Hello from BridgeLabz";
+  } 
 }
