17cc82af7a52c022b6680927f11ee1621206e4bb
DAXILSAXENA <daxildaxil@gmail.com>
2021-08-23 05:47:53 +0530

    [Daxil] Added:To display Hello from BridgeLabz with a user inputted name with Validation.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,8 @@ export class AppComponent {
   title = 'hello-world';
   imgUrl="";
   url="";
-  userName: String = "";
+  userName: string = "";
+  nameError: string = "";
 
   ngOnInit(): void {
     this.title = "Hello from BridgeLabz";
@@ -21,5 +22,14 @@ export class AppComponent {
   onClick($event: any) {
     console.log("Save button is clicked!", $event);
     window.open(this.url, "_blank");
+  }
+
+  onInput($event: any) {
+    const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    if (nameRegex.test(this.userName)) {
+      this.nameError = "";
+      return;
+    }
+    this.nameError = "Name is incorrect!";
   }
 }
